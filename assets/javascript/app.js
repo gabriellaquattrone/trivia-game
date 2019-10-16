fad5d199288e2a82ad9e288414753d27299dd5ea
Gabriella Quattrone <gfquattrone@ucdavis.edu>
2019-10-15 17:55:31 -0700

    Added Number Correct and Number Wrong. There is a bug when you keep clicking on an item.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,16 @@
-let questionsAndAnswers = [ 
-    { question: 'What is the word for this piece of Japanese clothing?', a: 'haori', b: 'kimono', c: 'batik', correct: 'b'},
-    { question: 'What is the tallest mountain in Japan?', a: 'Mount Fuji', b: 'Mount Haono', c: 'Mount Kaori', correct: 'a'},
-    { question: 'What was the most popular movie in Japan in 2002?', a: 'Sprited Away by Hayao Miyazaki', b: 'The Cat Returns by Hiroyuki Morita', c: 'Godzilla Against Mechagodzilla by Masaaki Tezuka', correct: 'a'},
-    { question: 'About how many vending machines are in Japan?', a: 'about 3 million', b: 'only 1 million', c: 'over 5 million', correct: 'c'},
-    { question: 'What is a festival in Japan called?', a: 'tegami', b: 'matsuri', c: 'ringo', correct: 'b'},
-    { question: 'Where is Tokyo Disneyland actually located?', a: 'In Tokyo, of course.', b: 'In Chiba, very near Tokyo. They use the name of Tokyo for tourist purposes.', c: 'In Kyoto.', correct: 'b'},
-    { question: 'What is the biggest industry in Japan?', a: 'Anime', b: 'Construction', c: 'Manga', correct: 'b'},
-    { question: 'The oldest novel in the world was written in Japan. What is it?', a: 'The Tale of Genji by Murasaki Shikibu', b: 'Pillow Sketches by Sei Shonagon', c: 'The Kojiki', correct: 'a'},
-    { question: 'Japan has a word for death from overwork. What is it?', a: 'isu', b: 'monogatari', c: 'karoushi', correct: 'c'},
-    { question: 'What can prevent you from being in a Japanese bathhouse?', a: 'Having a tattoo. It can associate you with the yakuza.', b: 'Putting the towel on your head while in the onsen.', c: 'Showering first before entering the bathhouse.', correct: 'a'}
+let questionsAndAnswers = [
+    { question: 'What is the word for this piece of Japanese clothing?', a: 'haori', b: 'kimono', c: 'batik', correct: 'b' },
+    { question: 'What is the tallest mountain in Japan?', a: 'Mount Fuji', b: 'Mount Haono', c: 'Mount Kaori', correct: 'a' },
+    { question: 'What was the most popular movie in Japan in 2002?', a: 'Sprited Away by Hayao Miyazaki', b: 'The Cat Returns by Hiroyuki Morita', c: 'Godzilla Against Mechagodzilla by Masaaki Tezuka', correct: 'a' },
+    { question: 'About how many vending machines are in Japan?', a: 'about 3 million', b: 'only 1 million', c: 'over 5 million', correct: 'c' },
+    { question: 'What is a festival in Japan called?', a: 'tegami', b: 'matsuri', c: 'ringo', correct: 'b' },
+    { question: 'Where is Tokyo Disneyland actually located?', a: 'In Tokyo, of course.', b: 'In Chiba, very near Tokyo. They use the name of Tokyo for tourist purposes.', c: 'In Kyoto.', correct: 'b' },
+    { question: 'What is the biggest industry in Japan?', a: 'Anime', b: 'Construction', c: 'Manga', correct: 'b' },
+    { question: 'The oldest novel in the world was written in Japan. What is it?', a: 'The Tale of Genji by Murasaki Shikibu', b: 'Pillow Sketches by Sei Shonagon', c: 'The Kojiki', correct: 'a' },
+    { question: 'Japan has a word for death from overwork. What is it?', a: 'isu', b: 'monogatari', c: 'karoushi', correct: 'c' },
+    { question: 'What can prevent you from being in a Japanese bathhouse?', a: 'Having a tattoo. It can associate you with the yakuza.', b: 'Putting the towel on your head while in the onsen.', c: 'Showering first before entering the bathhouse.', correct: 'a' }
 ]
-    
+
 let numberCorrect = 0;
 let numberWrong = 0;
 let number = 0;
@@ -18,42 +18,46 @@ let time = 15;
 let intervalID;
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('#start').on("click", function() {
+    $('#start').on("click", function () {
         $('#start').remove();
         displayQuestion();
         startTimer();
     });
 
-    $('#a').on("click", function() {
+    $('#a').on("click", function () {
         checkIfRightAnswer('a');
     });
 
-    $('#b').on("click", function() {
+    $('#b').on("click", function () {
         checkIfRightAnswer('b');
     });
 
-    $('#c').on("click", function() {
+    $('#c').on("click", function () {
         checkIfRightAnswer('c');
+    });
+
+    $('#restart').on("click", function () {
+        restartGame();
     });
 
 });
 
-function startTimer(){
+function startTimer() {
     $('#timer').show();
     $('#timer').text(time);
     intervalID = setInterval(count, 1000);
 }
-function resetTimer(){
+function resetTimer() {
     stop();
     time = 15;
     startTimer();
 }
-function count(){
+function count() {
     time--;
     $('#timer').text(time);
-    if (time <= 0){
+    if (time <= 0) {
         stop();
         $('#correct-incorrect').text("Jikan desu! Time's up! The correct answer is hilighted.");
         $('#' + questionsAndAnswers[number].correct).addClass("highlight");
@@ -63,7 +67,7 @@ function count(){
 function stop() {
     clearInterval(intervalID);
 }
-function goToNextQuestionAfterStopped(){
+function goToNextQuestionAfterStopped() {
     resetTimer();
     $('#correct-incorrect').empty();
 
@@ -73,36 +77,75 @@ function goToNextQuestionAfterStopped(){
     number++;
     displayQuestion();
 }
-function displayQuestion(){
+function displayQuestion() {
+    $('#question').show();
+    $('#a').show();
+    $('#b').show();
+    $('#c').show();
     $('#question').text(questionsAndAnswers[number].question);
     $('#a').text(questionsAndAnswers[number].a);
     $('#b').text(questionsAndAnswers[number].b);
-    $('#c').text(questionsAndAnswers[number].c);    
+    $('#c').text(questionsAndAnswers[number].c);
 }
 
-function displayNextQuestion(){
+function displayNextQuestion() {
+    $('#question').show();
+    $('#a').show();
+    $('#b').show();
+    $('#c').show();
     resetTimer();
     $('#' + questionsAndAnswers[number].correct).removeClass("highlight");
     number++
-    $('#correct-incorrect').text("");
-    $('#question').text(questionsAndAnswers[number].question);
-    $('#a').text(questionsAndAnswers[number].a);
-    $('#b').text(questionsAndAnswers[number].b);
-    $('#c').text(questionsAndAnswers[number].c);  
+    if (number >= questionsAndAnswers.length) {
+        displayEndScreen();
+    }
+    else {
+        $('#correct-incorrect').text("");
+        $('#question').text(questionsAndAnswers[number].question);
+        $('#a').text(questionsAndAnswers[number].a);
+        $('#b').text(questionsAndAnswers[number].b);
+        $('#c').text(questionsAndAnswers[number].c);
+    }
 }
-
+function displayEndScreen() {
+    $('#correct-incorrect').text("Congratulations! Here is your score.");
+    $('#num-correct-wrong').show();
+    $('#timer').hide();
+    stop();
+    $('#display-correct').show();
+    $('#display-wrong').show();
+    $('#restart').show();
+    $('#question').hide();
+    $('#a').hide();
+    $('#b').hide();
+    $('#c').hide();
+    $('#num-correct').text(numberCorrect);
+    $('#num-wrong').text(numberWrong);
+}
+function restartGame() {
+    $('#correct-incorrect').text("");
+    $('#num-correct-wrong').hide();
+    $('#restart').hide();
+    $('#display-correct').hide();
+    $('#display-wrong').hide();
+    $('#timer').show();
+    numberCorrect = 0;
+    numberWrong = 0;
+    number = 0;
+    displayQuestion();
+    startTimer();
+}
 function checkIfRightAnswer(userAnswer) {
     if (questionsAndAnswers[number].correct == userAnswer) {
         $('#correct-incorrect').text("Pin-pon! You got it right.");
+        $('#question').hide();
+        $('#a').hide();
+        $('#b').hide();
+        $('#c').hide();
         numberCorrect++;
         console.log("Number: ", number, "Question Array Length: ", questionsAndAnswers.length);
-        if(number < questionsAndAnswers.length){
-           stop();
-           $('#' + questionsAndAnswers[number].correct).addClass("highlight");
-            // Make sure to let everything else run.
-           setTimeout(displayNextQuestion, 5000);
-
-        } 
+        // Make sure to let the stuff above this if statement run first.
+        setTimeout(displayNextQuestion, 1000);
     }
     else if (questionsAndAnswers[number].correct != userAnswer) {
         stop();
@@ -110,8 +153,6 @@ function checkIfRightAnswer(userAnswer) {
         numberWrong++;
         // Helped me with below: https://api.jquery.com/first/#entry-examples
         $('#' + questionsAndAnswers[number].correct).addClass("highlight");
-        if(number < questionsAndAnswers.length){
-            setTimeout(displayNextQuestion, 5000);
-        }    
+        setTimeout(displayNextQuestion, 5000);
     }
 }
